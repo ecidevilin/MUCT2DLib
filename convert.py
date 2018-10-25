4a11a7bd4e7520a968145c9f45bdbcddffd5f34e
Chaos <ecidevilin@live.cn>
2018-10-25 20:46:59 +0800

    mirror

--- convert.py
+++ convert.py
@@ -1,14 +1,27 @@
-
+import dlib
+from skimage import io
+import os
 rf = open('muct76-opencv.csv', "r")
 wf = open("training_with_face_landmarks.xml", "w")
 
+detector = dlib.get_frontal_face_detector()
 lines = rf.readlines()
 wf.writelines("<?xml version='1.0' encoding='ISO-8859-1'?>\n<?xml-stylesheet type='text/xsl' href='image_metadata_stylesheet.xsl'?>\n<dataset>\n<name>Training faces</name>\n<comment>http://www.milbo.org/muct</comment>\n<images>\n")
 for i in range(1, len(lines)):
-    if (lines[i].startswith("ir")):
-        continue
+    mirror = lines[i].startswith("ir")
     cols = lines[i].split(",")
-    wf.write("  <image file='{name}.jpg'>\n".format(name=cols[0]))
+    file = cols[0]
+    wf.write("  <image file='{name}.jpg'>\n".format(name=file))
+    if mirror:
+        if not os.path.exists("jpg/{name}.jpg".format(name=file)):
+            img = io.imread("jpg/{name}.jpg".format(name='i'+file[2:]))
+            img = img[:, ::-1]
+            img = io.imsave("jpg/{name}.jpg".format(name=file),img)
+            img = io.imread("jpg/{name}.jpg".format(name=file))
+        else:
+            img = io.imread("jpg/{name}.jpg".format(name=file))
+    else:
+        img = io.imread("jpg/{name}.jpg".format(name=file))
     xmax = 0
     ymax = 0
     xmin = 65535
@@ -20,6 +33,14 @@ for i in range(1, len(lines)):
         xmin = min(xmin, numx)
         ymax = max(ymax, numy)
         ymin = min(ymin, numy)
+    dets = detector(img, 1)
+    if (len(dets) == 0):
+        print(cols[0])
+    else:
+        ymin = min(ymin, dets[0].top())
+        xmin = min(xmin, dets[0].left())
+        ymax = max(ymax, dets[0].bottom())
+        xmax = max(xmax, dets[0].right())
     wf.write("    <box top='{top}' left='{left}' width='{width}' height='{height}'>\n".format(top=ymin,left=xmin,width=xmax-xmin+1,height=ymax-ymin+1))
     for j in range(1, int(len(cols) / 2)):
         numx = int(round(float(cols[j * 2])))
